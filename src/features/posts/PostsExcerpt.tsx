5d261bc17f5e5d4f6fb07b345723b2ebebe25e91
Taras <hrynchuktt@gmail.com>
2024-01-23 19:04:03 +0200

    bugfix

--- src/features/posts/PostsExcerpt.tsx
+++ src/features/posts/PostsExcerpt.tsx
@@ -13,7 +13,7 @@ const PostsExcerpt: FC<{ postId: number | string }> = memo(({ postId }) => {
    return (
       <article>
          <h2>{post.title}</h2>
-         <p className="excerpt">{post.body.substring(0, 75)}...</p>``
+         <p className="excerpt">{post.body.substring(0, 75)}...</p>
          <p className="postCredit">
             <Link to={`post/${post.id}`} >View Post</Link>
             <PostAuthor userId={post.userId} />
